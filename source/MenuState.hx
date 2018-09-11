3f92d558558e93c371efe78c10c179164028558a
Alex <alexloon123@gmail.com>
2018-09-11 14:16:16 -0400

    Added empty project

---
+++ source/MenuState.hx
@@ -0,0 +1,27 @@
+package;
+
+import flixel.FlxState;
+import flixel.ui.FlxButton;
+import flixel.FlxG;
+
+class MenuState extends FlxState
+{
+	var _playButton: FlxButton;
+	
+	override public function create():Void
+	{
+		super.create();
+		_playButton = new FlxButton(0, 0, "P L A Y", clickPlay);
+		_playButton.screenCenter();
+		add(_playButton);
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+	
+	function clickPlay():Void{
+		FlxG.switchState(new PlayState());
+	}
+}
