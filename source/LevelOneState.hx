bf34313509effd62c022f43e5b75cd75483cffb6
Jared Tesone <nyrfanjp3@gmail.com>
2018-09-16 14:59:31 -0400

    Player support for level one state

--- source/LevelOneState.hx
+++ source/LevelOneState.hx
@@ -20,16 +20,23 @@ class LevelOneState extends FlxState {
 		_mWalls = new FlxTilemap();
 		_mSpikes = new FlxTilemap();
 		_mFan = new FlxTilemap();
-		_mWalls.loadMapFromArray(cast(_map.getLayer("walls"), TiledTileLayer).tileArray, _map.width, _map.height, 
+		_mWalls.loadMapFromArray(cast(_map.getLayer("Walls"), TiledTileLayer).tileArray, _map.width, _map.height, 
 			AssetPaths.assets1stMap_tsx, _map.tileWidth, _map.tileHeight, FlxTilemapAutoTiling.OFF, 1, 1, 3);
-		_mSpikes.loadMapFromArray(cast(_map.getLayer("spikes"), TiledTileLayer).tileArray, _map.width, _map.height, 
+		_mWalls.setTileProperties()
+/*		_mSpikes.loadMapFromArray(cast(_map.getLayer("spikes"), TiledTileLayer).tileArray, _map.width, _map.height, 
 			AssetPaths.assets1stMap_tsx, _map.tileWidth, _map.tileHeight, FlxTilemapAutoTiling.OFF, 1, 1, 3);
 		_mFan.loadMapFromArray(cast(_map.getLayer("fans"), TiledTileLayer).tileArray, _map.width, _map.height, 
 			AssetPaths.assets1stMap_tsx, _map.tileWidth, _map.tileHeight, FlxTilemapAutoTiling.OFF, 1, 1, 3);
-
+		*/_mWalls.follow();
+		//collision directions for walls, spikes, and fans
+		add(_mWalls);
 
 		//pass 0 for frog, 1 for elephant
-		_player = new Player(20, 20, 0);
+		_player = new Player(0);
+		var tmpMap:TiledObjectLayer = cast _map.getLayer("entities");
+		for (e in tmpMap.objects) {
+			placeEntities(e.type, e.xmlData.x);
+		}
 		add(_player);
 		super.create();
 	}
@@ -37,5 +44,15 @@ class LevelOneState extends FlxState {
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		FlxG.collide(_player, _mWalls);
+	}
+
+	function placeEntities(entityName:String, entityData.Xml):Void {
+		var x:Int = Std.parseInt(entityData.get("x"));
+		var y:Int = Std.parseInt(entityData.get("y"));
+		if (entityName == "player") {
+			_player.x = x;
+			_player.y = y;
+		}
 	}
 }
