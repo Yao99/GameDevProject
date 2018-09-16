9fbc948820ccd08f1ee05aa26b663e17164ed32b
Jared Tesone <nyrfanjp3@gmail.com>
2018-09-16 15:16:01 -0400

    Fixed tileset file reference

--- assets/data/Assets1stMap.tsx
+++ assets/data/Assets1stMap.tsx
@@ -2,60 +2,60 @@
 <tileset name="assets" tilewidth="75" tileheight="150" tilecount="19" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="75" height="75" source="../../data/PlatformAssets/CollapseBallonPart1.png"/>
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart1.png"/>
  </tile>
  <tile id="1">
-  <image width="75" height="75" source="../../data/PlatformAssets/CollapseBallonPart2.png"/>
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart2.png"/>
  </tile>
  <tile id="2">
-  <image width="75" height="75" source="../../data/PlatformAssets/CollapseBallonPart3.png"/>
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart3.png"/>
  </tile>
  <tile id="3">
-  <image width="75" height="75" source="../../data/PlatformAssets/CollapseBallonPart4.png"/>
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart4.png"/>
  </tile>
  <tile id="4">
-  <image width="75" height="75" source="../../data/PlatformAssets/CollapseBallonPart5.png"/>
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart5.png"/>
  </tile>
  <tile id="5">
-  <image width="75" height="75" source="../../data/PlatformAssets/CollapseBallonPart6.png"/>
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart6.png"/>
  </tile>
  <tile id="6">
-  <image width="75" height="75" source="../../data/PlatformAssets/EPlatformPart1.png"/>
+  <image width="75" height="75" source="PlatformAssets/EPlatformPart1.png"/>
  </tile>
  <tile id="7">
-  <image width="75" height="75" source="../../data/PlatformAssets/EPlatformPart2.png"/>
+  <image width="75" height="75" source="PlatformAssets/EPlatformPart2.png"/>
  </tile>
  <tile id="9">
-  <image width="75" height="75" source="../../data/PlatformAssets/MPlatformPart1.png"/>
+  <image width="75" height="75" source="PlatformAssets/MPlatformPart1.png"/>
  </tile>
  <tile id="10">
-  <image width="75" height="75" source="../../data/PlatformAssets/Spikes.png"/>
+  <image width="75" height="75" source="PlatformAssets/Spikes.png"/>
  </tile>
  <tile id="11">
-  <image width="75" height="75" source="../../data/PlatformAssets/SupportBallonPart1.png"/>
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart1.png"/>
  </tile>
  <tile id="12">
-  <image width="75" height="75" source="../../data/PlatformAssets/SupportBallonPart2.png"/>
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart2.png"/>
  </tile>
  <tile id="13">
-  <image width="75" height="75" source="../../data/PlatformAssets/SupportBallonPart3.png"/>
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart3.png"/>
  </tile>
  <tile id="14">
-  <image width="75" height="75" source="../../data/PlatformAssets/SupportBallonPart4.png"/>
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart4.png"/>
  </tile>
  <tile id="15">
-  <image width="75" height="75" source="../../data/PlatformAssets/SupportBallonPart5.png"/>
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart5.png"/>
  </tile>
  <tile id="16">
-  <image width="75" height="75" source="../../data/PlatformAssets/SupportBallonPart6.png"/>
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart6.png"/>
  </tile>
  <tile id="17">
-  <image width="75" height="75" source="../../data/PlatformAssets/WPlatformPart1.png"/>
+  <image width="75" height="75" source="PlatformAssets/WPlatformPart1.png"/>
  </tile>
  <tile id="18">
-  <image width="75" height="150" source="../../data/PlatformAssets/FloatySpike.png"/>
+  <image width="75" height="150" source="PlatformAssets/FloatySpike.png"/>
  </tile>
  <tile id="19">
-  <image width="75" height="75" source="../../data/PlatformAssets/Key.png"/>
+  <image width="64" height="48" source="PlatformAssets/Key.png"/>
  </tile>
 </tileset>
