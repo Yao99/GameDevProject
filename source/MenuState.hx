d2483bb95a3ba54b8cb2004c67ede50954d5a487
Giovanni Morales <GiovanniMorales021@gmail.com>
2018-09-18 14:36:02 -0400

    Main Menu Music works now!

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -10,6 +10,10 @@ class MenuState extends FlxState
 	
 	override public function create():Void
 	{
+		if (FlxG.sound.music == null) // don't restart the music if it's already playing
+ {
+     FlxG.sound.playMusic(AssetPaths.mainMenu__wav, 1, true);
+ }
 		super.create();
 		_playButton = new FlxButton(0, 0, "P L A Y", clickPlay);
 		_playButton.screenCenter();
@@ -24,4 +28,5 @@ class MenuState extends FlxState
 	function clickPlay():Void{
 		FlxG.switchState(new LevelOneState());
 	}
+	
 }
