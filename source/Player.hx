847489d29f3cdfd59822b8090c59c1d55f3aa075
Alex <alexloon123@gmail.com>
2018-09-18 15:10:15 -0400

    player changes / species switching

--- source/Player.hx
+++ source/Player.hx
@@ -11,47 +11,29 @@
  class Player extends FlxSprite {
 	 
 	//movement
- 	public var speed_limit:Float = 200;	
- 	public var gravity:Float = 200;
-	public var xVelocity:Float = 0;
-    public var yVelocity:Float = 0;
+ 	public var speed_limit:Float = 1000;	
+ 	public var gravity:Float = 500;
     public var touchingFloor:Bool = true;
+	public var jumping:Bool = false;
+	
+	//species
+	var species:Int;
 
-    public function new(?X:Float=0, ?Y:Float=0, species:Float) {
+    public function new(?X:Float=0, ?Y:Float=0, s:Int) {
          super(X, Y);
-         if (species == 0) {
-         	loadGraphic("assets/images/Frog1.png", true, 81, 85);
-         	setFacingFlip(FlxObject.LEFT, true, false);
-         	setFacingFlip(FlxObject.RIGHT, false, false);
-         	animation.add("walk", [0, 1, 2], 4, true);
-         	animation.add("idle", [0, 0, 0, 3], 1, false);
-         } else if (species == 1) {
-         	loadGraphic("assets/images/Elephant.png", true, 137, 125);
-         	setFacingFlip(FlxObject.LEFT, true, false);
-         	setFacingFlip(FlxObject.RIGHT, false, false);
-         	animation.add("walk", [0, 2, 10], 3, true);
-         	animation.add("idle", [10, 10, 10, 0, 0], 1, false);
-         }
-		 /*loadGraphic("assets/images/duck.png", true, 100, 114);
-		 setFacingFlip(FlxObject.LEFT, true, false);
-		 setFacingFlip(FlxObject.RIGHT,false,false);
-		 animation.add("walk", [0, 3, 0, 2], 10, true);
-		 animation.play("walk");
-		 facing = FlxObject.LEFT;*/
+		 species = s;
+		 speciesSetup();
 
-		 //make hitbox
-		 width = 75;
-		 height = 75;
 		 //offset.y = 5;
 		 //updateHitbox();
 
-		 maxVelocity.set(160, 400);
+		 maxVelocity.set(250, 750);
 		 acceleration.y = gravity;
 		 drag.x = maxVelocity.x * 4;
 
      }
 	 
-     function movement():Void {
+    function movement():Void {
      	var _left = false;
      	var _right = false;
      	var _jump = false;
@@ -60,25 +42,37 @@
  		_left = FlxG.keys.anyPressed([LEFT, A]);
  		_right = FlxG.keys.anyPressed([RIGHT, D]);
  		_jump = FlxG.keys.justPressed.SPACE;
-		
-		acceleration.x = 0;
-		
 
- 		//cancel opposing directions
- 		/*if (_left && _right)
- 			_left = _right = false;*/
+ 		if (FlxG.keys.justPressed.ONE){
+			if (species != 0){
+				species = 0;
+				speciesSetup();
+			}
+		}
+		if (FlxG.keys.justPressed.TWO){
+			if (species != 1){
+				species = 1;
+				speciesSetup();
+			}
+		}
+		if (FlxG.keys.justPressed.THREE){
+			if (species != 2){
+				species = 2;
+				speciesSetup();
+			}
+		}
 
  		if (_left || _right || _jump) {
  			if (_jump && touchingFloor) {
- 				velocity.y = -maxVelocity.y / 2;
+ 				velocity.y = -750;
  			}
 
  			if (_left && !_right) {
- 				acceleration.x = -maxVelocity.x * 4;
+ 				velocity.x = FlxMath.lerp(velocity.x, -speed_limit, .5);
  				facing = FlxObject.LEFT;
  				animation.play("walk");
  			} else if (_right && !_left) {
- 				acceleration.x = maxVelocity.x * 4;
+ 				velocity.x = FlxMath.lerp(velocity.x, speed_limit, .5);
  				facing = FlxObject.RIGHT;
  				animation.play("walk");
  			} else {
@@ -93,40 +87,40 @@
  			y = 2475;
  		}
 		
-		//directions
-		/*if (_left) {
-			xVelocity = FlxMath.lerp(xVelocity, -speed_limit, .5);
-			velocity.rotate(FlxPoint.weak(0,0), 180);
-			facing = FlxObject.LEFT;
-			animation.play("walk");				
-		} else if (_right) {
-		    xVelocity = FlxMath.lerp(xVelocity, speed_limit, .5);
-			velocity.rotate(FlxPoint.weak(0,0), 0);
-			facing = FlxObject.RIGHT;
-			animation.play("walk");			
-		} else{
-			xVelocity = FlxMath.lerp(xVelocity, 0, .9);
-		}
+		velocity.y = FlxMath.lerp(velocity.y, speed_limit, .01);
 		
-		if (_jump) {
-			yVelocity = -600;
-			facing = FlxObject.LEFT;
-			animation.play("walk");
-		}
-		
-		if (!_jump && !_left && !_right){
-			animation.play("idle");
-		}
-
-		if (touchingFloor) {
-			velocity.y = 0;
-			acceleration.y = 0;
-		} else {
-			//acceleration.y = gravity;
-			acceleration.y = gravity;
-		}*/
     }
 
+	function speciesSetup():Void{
+		if(touchingFloor)
+		y -= 50;
+		 if (species == 0) {
+         	loadGraphic("assets/images/Frog1.png", true, 81, 85);
+         	setFacingFlip(FlxObject.LEFT, true, false);
+         	setFacingFlip(FlxObject.RIGHT, false, false);
+         	animation.add("walk", [0, 1, 2], 4, true);
+         	animation.add("idle", [0, 0, 0, 3], 1, false);
+			width = 81;
+			height = 85;
+         } else if (species == 1) {
+         	loadGraphic("assets/images/Elephant.png", true, 137, 125);
+         	setFacingFlip(FlxObject.LEFT, true, false);
+         	setFacingFlip(FlxObject.RIGHT, false, false);
+         	animation.add("walk", [0, 2, 10], 3, true);
+         	animation.add("idle", [10, 10, 10, 0, 0], 1, false);
+			width = 137;
+			height = 125;
+         }else if (species == 2) {
+         	loadGraphic("assets/images/squirrelGlide.png", true, 87, 100);
+         	setFacingFlip(FlxObject.LEFT, true, false);
+         	setFacingFlip(FlxObject.RIGHT, false, false);
+         	animation.add("walk", [0, 1, 2], 3, true);
+         	animation.add("idle", [0], 1, false);
+			width = 87;
+			height = 100;
+         }
+	}
+	
     override public function update(elapsed:Float):Void {
     	movement();
     	super.update(elapsed);
