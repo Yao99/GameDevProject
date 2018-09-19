d57efac9f5881ea318d38b953438cba2e951328d
William Yao <willyao99@gmail.com>
2018-09-19 13:43:38 -0400

    Updated tilemaps and added final tileset

---
+++ assets/data/tilesetfinal.tsx
@@ -0,0 +1,121 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tilesetfinal" tilewidth="75" tileheight="75" tilecount="39" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart1 - Copy.png"/>
+ </tile>
+ <tile id="1">
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart1v3 - Copy.png"/>
+ </tile>
+ <tile id="3">
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart1v3.png"/>
+ </tile>
+ <tile id="4">
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart1v4 - Copy.png"/>
+ </tile>
+ <tile id="5">
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart1v4.png"/>
+ </tile>
+ <tile id="6">
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart2 - Copy.png"/>
+ </tile>
+ <tile id="7">
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart2.png"/>
+ </tile>
+ <tile id="8">
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart3 - Copy.png"/>
+ </tile>
+ <tile id="9">
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart3.png"/>
+ </tile>
+ <tile id="10">
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart3v3 - Copy.png"/>
+ </tile>
+ <tile id="11">
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart3v3.png"/>
+ </tile>
+ <tile id="12">
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart3v4.png"/>
+ </tile>
+ <tile id="13">
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart4.png"/>
+ </tile>
+ <tile id="14">
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart5.png"/>
+ </tile>
+ <tile id="15">
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart6 - Copy.png"/>
+ </tile>
+ <tile id="16">
+  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart6.png"/>
+ </tile>
+ <tile id="17">
+  <image width="75" height="75" source="PlatformAssets/CollapseBalloonPart1v2 - Copy.png"/>
+ </tile>
+ <tile id="18">
+  <image width="75" height="75" source="PlatformAssets/CollapseBalloonPart1v2.png"/>
+ </tile>
+ <tile id="19">
+  <image width="75" height="75" source="PlatformAssets/Spikes.png"/>
+ </tile>
+ <tile id="20">
+  <image width="75" height="75" source="PlatformAssets/Spikesv2.png"/>
+ </tile>
+ <tile id="21">
+  <image width="75" height="75" source="PlatformAssets/Spikesv3.png"/>
+ </tile>
+ <tile id="22">
+  <image width="75" height="75" source="PlatformAssets/Spikesv4.png"/>
+ </tile>
+ <tile id="23">
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart1.png"/>
+ </tile>
+ <tile id="24">
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart1v2.png"/>
+ </tile>
+ <tile id="25">
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart1v4.png"/>
+ </tile>
+ <tile id="26">
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart2.png"/>
+ </tile>
+ <tile id="27">
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart3.png"/>
+ </tile>
+ <tile id="28">
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart3v2.png"/>
+ </tile>
+ <tile id="29">
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart3v3.png"/>
+ </tile>
+ <tile id="30">
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart3v4.png"/>
+ </tile>
+ <tile id="31">
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart4.png"/>
+ </tile>
+ <tile id="32">
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart5.png"/>
+ </tile>
+ <tile id="33">
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart5v2.png"/>
+ </tile>
+ <tile id="34">
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart5v3.png"/>
+ </tile>
+ <tile id="35">
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart5v4.png"/>
+ </tile>
+ <tile id="36">
+  <image width="75" height="75" source="PlatformAssets/SupportBallonPart6.png"/>
+ </tile>
+ <tile id="37">
+  <image width="75" height="75" source="PlatformAssets/SupportBalloonPart1v3.png"/>
+ </tile>
+ <tile id="38">
+  <image width="75" height="75" source="PlatformAssets/FloatySpikeHalf.png"/>
+ </tile>
+</tileset>
