23271f9556d9dea5f00cde5ec2ddff980ae7d06d
William Yao <willyao99@gmail.com>
2018-09-19 15:21:50 -0400

    Recolored tilemap1 with new tileset

--- assets/data/tilesetfinal.tsx
+++ assets/data/tilesetfinal.tsx
@@ -1,14 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tilesetfinal" tilewidth="75" tileheight="75" tilecount="39" columns="0">
+<tileset name="tilesetfinal" tilewidth="75" tileheight="75" tilecount="31" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="75" height="75" source="PlatformAssets/CollapseBallonPart1 - Copy.png"/>
- </tile>
- <tile id="1">
-  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart1.png"/>
- </tile>
- <tile id="2">
-  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart1v3 - Copy.png"/>
  </tile>
  <tile id="3">
   <image width="75" height="75" source="PlatformAssets/CollapseBallonPart1v3.png"/>
@@ -16,26 +10,14 @@
  <tile id="4">
   <image width="75" height="75" source="PlatformAssets/CollapseBallonPart1v4 - Copy.png"/>
  </tile>
- <tile id="5">
-  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart1v4.png"/>
- </tile>
  <tile id="6">
   <image width="75" height="75" source="PlatformAssets/CollapseBallonPart2 - Copy.png"/>
- </tile>
- <tile id="7">
-  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart2.png"/>
  </tile>
  <tile id="8">
   <image width="75" height="75" source="PlatformAssets/CollapseBallonPart3 - Copy.png"/>
  </tile>
- <tile id="9">
-  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart3.png"/>
- </tile>
  <tile id="10">
   <image width="75" height="75" source="PlatformAssets/CollapseBallonPart3v3 - Copy.png"/>
- </tile>
- <tile id="11">
-  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart3v3.png"/>
  </tile>
  <tile id="12">
   <image width="75" height="75" source="PlatformAssets/CollapseBallonPart3v4.png"/>
@@ -48,12 +30,6 @@
  </tile>
  <tile id="15">
   <image width="75" height="75" source="PlatformAssets/CollapseBallonPart6 - Copy.png"/>
- </tile>
- <tile id="16">
-  <image width="75" height="75" source="PlatformAssets/CollapseBallonPart6.png"/>
- </tile>
- <tile id="17">
-  <image width="75" height="75" source="PlatformAssets/CollapseBalloonPart1v2 - Copy.png"/>
  </tile>
  <tile id="18">
   <image width="75" height="75" source="PlatformAssets/CollapseBalloonPart1v2.png"/>
